f134290f4fd23cbcdb1c55b38560ac2432267353
Anna Reznikova <reznic11@rambler.ru>
2013-01-28 10:30:30 +0400

    comit2

--- calculator4/calculator4.cpp
+++ calculator4/calculator4.cpp
@@ -77,8 +77,8 @@ int OPResult(const int left, const char op, const int right) {
 	}
 	return result;
 }
-template<class T>
-void FindNum(stack <T>* &myStack2 , int& i, char* a){
+
+int FindNum( int& i, char* a){
 	int ch = 0;
 	int numb = 0;
 	int razr = 1;
@@ -88,11 +88,7 @@ void FindNum(stack <T>* &myStack2 , int& i, char* a){
 		razr = 0xA;
 		i++;
 	}
-	myStack2 = push(myStack2,numb);
-
-	numb=0;
-	razr=1;
-	i--;
+	return numb;
 }
 int Prior_Op(char el, int & prior_op){
 	switch (el) {
@@ -108,88 +104,39 @@ int Prior_Op(char el, int & prior_op){
 					return prior_op = 2;
 	}
 }
-//template<class T>
-//void FindOperator (int &prior_op, int &prior_op2, stack <T>* &myStack2, stack <T>* &myStack1, char*a , int& i, int result){
-//	if(prior_op>prior_op2 && prior_op!=4 || prior_op == 1){myStack1 = push(myStack1, a[i]);
-//			}
-//	if(prior_op<=prior_op2 && prior_op != 1 || prior_op==4){
-//					if(myStack1!=NULL){
-//						int b = pick(myStack2);
-//						myStack2 = pop(myStack2);
-//						int a = pick(myStack2);
-//						myStack2 = pop(myStack2);
-//						result = OPResult(a , pick(myStack1), b);
-//					}
-//					myStack2=push(myStack2,result);
-//					if(myStack1!=NULL){myStack1=pop(myStack1);
-//					}
-//					if(myStack1!=NULL){
-//						if(pick(myStack1)=='+'|| pick(myStack1)=='-'){prior_op2 = 2;}
-//						if(pick(myStack1)=='*'|| pick(myStack1)=='/'){prior_op2 = 3;}
-//						if(pick(myStack1)==')'){prior_op2 = 4;}
-//						if(pick(myStack1)=='('){prior_op2 = 1;}
-//						prior_op=prior_op2;
-//						i--;
-//						if(a[i+1]==')' && pick(myStack1)=='(' ){
-//							myStack1=pop(myStack1);
-//							prior_op=0;
-//							prior_op2 = prior_op;
-//							i++;
-//						}
-//					}
-//			/*}*/
-//		}
-//}
-int main(){
-	int countStack1 = 0;
-	int memCountSt1 = 0;
-	stack<char>* myStack1=NULL;
-	stack<int>* myStack2=NULL;
-	char *a = "( 9+(3+1))-6";
-	const int n = strlen(a);
-	int prior_op = 0; 
-	int prior_op2 = 0;
-	int result = 0;
-	for(int i=0; i<n; i++){
-		if (a[i] == 0x20){ i++;
+
+void FindOperator (int &prior_op, int &prior_op2, stack <int>* &myStack2, stack <char>* &myStack1, char*a , int& i, int result){
+	if(prior_op>prior_op2 && prior_op!=4 || prior_op == 1){myStack1 = push(myStack1, a[i]);
+	}
+	if(prior_op<=prior_op2 && prior_op != 1 || prior_op==4){
+		if(myStack1!=NULL){
+			int b = pick(myStack2);
+			myStack2 = pop(myStack2);
+			int a = pick(myStack2);
+			myStack2 = pop(myStack2);
+			result = OPResult(a , pick(myStack1), b);
 		}
-		if(a[i] >= '0'){
-			FindNum(myStack2 ,i, a);
-			prior_op2 = prior_op;
+		myStack2=push(myStack2,result);
+		if(myStack1!=NULL){myStack1=pop(myStack1);
 		}
-		if(a[i]<='/'){
-			Prior_Op(a[i], prior_op);		
-			//FindOperator (prior_op, prior_op2, myStack2, myStack1, a , i, result );*/
-			if(prior_op>prior_op2 && prior_op!=4 || prior_op == 1){myStack1 = push(myStack1, a[i]);
-			}
-			if(prior_op<=prior_op2 && prior_op != 1 || prior_op==4){
-				if(myStack1!=NULL){
-					int b = pick(myStack2);
-					myStack2 = pop(myStack2);
-					int a = pick(myStack2);
-					myStack2 = pop(myStack2);
-					result = OPResult(a , pick(myStack1), b);
-				}
-				myStack2=push(myStack2,result);
-				if(myStack1!=NULL){myStack1=pop(myStack1);
-				}
-				if(myStack1!=NULL){
-					if(pick(myStack1)=='+'|| pick(myStack1)=='-'){prior_op2 = 2;}
-					if(pick(myStack1)=='*'|| pick(myStack1)=='/'){prior_op2 = 3;}
-					if(pick(myStack1)==')'){prior_op2 = 4;}
-					if(pick(myStack1)=='('){prior_op2 = 1;}
-					prior_op=prior_op2;
-					i--;
-					if(a[i+1]==')' && pick(myStack1)=='(' ){
-						myStack1=pop(myStack1);
-						prior_op=0;
-						prior_op2 = prior_op;
-						i++;
-					}
-				}
+		if(myStack1!=NULL){
+			if(pick(myStack1)=='+'|| pick(myStack1)=='-'){prior_op2 = 2;}
+			if(pick(myStack1)=='*'|| pick(myStack1)=='/'){prior_op2 = 3;}
+			if(pick(myStack1)==')'){prior_op2 = 4;}
+			if(pick(myStack1)=='('){prior_op2 = 1;}
+			prior_op=prior_op2;
+			i--;
+			if(a[i+1]==')' && pick(myStack1)=='(' ){
+				myStack1=pop(myStack1);
+				prior_op=0;
+				prior_op2 = prior_op;
+				i++;
 			}
 		}
 	}
+}
+void FinishCheck( stack<char>* &myStack1, stack<int>* &myStack2){
+	int result = 0;
 	if(myStack1!=NULL){
 		int b = pick(myStack2);
 		myStack2 = pop(myStack2);
@@ -199,6 +146,36 @@ int main(){
 		myStack2=push(myStack2,result);
 		myStack1=pop(myStack1);
 	}
+}
+int main(){
+	int countStack1 = 0;
+	int memCountSt1 = 0;
+	stack<char>* myStack1=NULL;
+	stack<int>* myStack2=NULL;
+	char *a = "( 9+(3+1))-  6";
+	const int n = strlen(a);
+	int prior_op = 0; 
+	int prior_op2 = 0;
+	int result = 0;
+	for(int i=0; i<n; i++){
+		if (a[i] == 0x20){ while(a[i]==0x20){ i++;
+		}
+		}
+		if(a[i] >= '0'){
+			int numb = FindNum(i, a);
+			myStack2 = push(myStack2,numb);
+			prior_op2 = prior_op;
+		}
+		if(a[i]<='/'){
+			Prior_Op(a[i], prior_op);	
+			FindOperator (prior_op, prior_op2, myStack2, myStack1, a , i, result);
+		}
+		else{
+			cout<< "There are some bugs!!! " << "  ";
+			break;
+		}
+	}
+	FinishCheck(myStack1 , myStack2 );		
 	cout<< " the answer is  -   ";
 	list_print(myStack2);
 	system("pause");
